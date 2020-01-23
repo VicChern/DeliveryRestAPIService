e73823967cc67c09029437a420fff1504efbd47b
Ihor Zahoruiko <igorvladz@gmail.com>
2020-01-23 03:04:40 +0200

    UserController: 1.used Mapping annotations, 2.modified response construction

--- src/main/java/com/softserve/itacademy/kek/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/UserController.java
@@ -1,99 +1,81 @@
 package com.softserve.itacademy.kek.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.json.JSONObject;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController extends DefaultController {
-    private Map<String, String> response = new HashMap<>();
+
+    // Build Response (temporary method)
+    private String getResponse(String id, String status) {
+        JSONObject response = new JSONObject();
+        response.put("userID", id);
+        response.put("status", status);
+        return response.toString();
+    }
+
+    /**
+     * Get information about users
+     *
+     * @return information about users (JSON)
+     */
+    @GetMapping
+    public ResponseEntity<String> getUserList() {
+        JSONObject response = new JSONObject();
+        response.append("userID", "1").append("userID", "2").append("userID", "3");
+        response.put("status", "received");
+        return ResponseEntity.ok(response.toString());
+    }
 
     /**
      * Returns information about the requested user
+     *
      * @param id user ID from the URN
      * @return user information as a JSON
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET,
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public Map<String, String> getUser(@PathVariable String id) {
-        response.clear();
-
-        try {
-            Integer.parseInt(id);
-
-            response.put("User ID", id);
-            response.put("User status", "received");
-        } catch (NumberFormatException e) {
-            response.put("Error", "Could not receive the user");
-            response.put("Error details", "Incorrect user ID format");
-        }
-
-        return response;
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getUser(@PathVariable String id) {
+        return ResponseEntity.ok(getResponse(id, "received"));
     }
 
     /**
      * Creates a new user
+     *
      * @return operation status as a JSON
      */
-    @RequestMapping(method = RequestMethod.POST,
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public Map<String, String> addUser() {
-        response.clear();
-
-        response.put("User ID", "1");
-        response.put("User status", "added");
-        return response;
+    @PostMapping
+    public ResponseEntity<String> addUser() {
+        return ResponseEntity.ok(getResponse("new", "added"));
     }
 
     /**
      * Modifies information of the specified user
+     *
      * @param id user ID from the URN
      * @return operation status as a JSON
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT,
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public Map<String, String> modifyUser(@PathVariable String id) {
-        response.clear();
-
-        try {
-            Integer.parseInt(id);
-
-            response.put("User ID", id);
-            response.put("User status", "modified");
-        } catch (NumberFormatException e) {
-            response.put("Error", "Could not modify the user");
-            response.put("Error details", "Incorrect user ID format");
-        }
-
-        return response;
+    @PutMapping("/{id}")
+    public ResponseEntity<String> modifyUser(@PathVariable String id) {
+        return ResponseEntity.ok(getResponse(id,"modified"));
     }
 
     /**
      * Removes the specified user
+     *
      * @param id user ID from the URN
      * @return operation status as a JSON
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE,
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public Map<String, String> removeUser(@PathVariable String id) {
-        response.clear();
-
-        try {
-            Integer.parseInt(id);
-
-            response.put("User ID", id);
-            response.put("User status", "removed");
-        } catch (NumberFormatException e) {
-            response.put("Error", "Could not remove the user");
-            response.put("Error details", "Incorrect user ID format");
-        }
-
-        return response;
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable String id) {
+        return ResponseEntity.ok(getResponse(id, "deleted"));
     }
 }
