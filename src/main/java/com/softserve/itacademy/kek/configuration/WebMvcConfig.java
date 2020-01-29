c7b5bc6a10a371adacf2ddd7a6abd3a947dbdd5a
Olexiy Lyaschenko <lyaschenko2021@gmail.com>
2020-01-29 14:14:33 +0200

    [20] Added WebSecurityConfig, SecurityWebApplicationInitializer, MvcWebApplicationInitializer, Auth0Controller, but SpringSecurity doesn't work with Embedded Tomcat, also added spring.security.config, spring.security.web dependencies

--- src/main/java/com/softserve/itacademy/kek/configuration/WebMvcConfig.java
+++ src/main/java/com/softserve/itacademy/kek/configuration/WebMvcConfig.java
@@ -3,17 +3,21 @@ package com.softserve.itacademy.kek.configuration;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig implements WebMvcConfigurer {
+@Import(WebSecurityConfig.class)
+@ComponentScan(basePackages = {"com.softserve.itacademy.kek", "com.softserve.itacademy.kek.configuration", "com.softserve.itacademy.kek.controller"})
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -29,6 +33,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         final Map<String, String> parameterMap = new HashMap<>();
         parameterMap.put("charset", "UTF-8");
         configurer.defaultContentType(new MediaType(MediaType.APPLICATION_JSON, parameterMap));
-
     }
+
 }
