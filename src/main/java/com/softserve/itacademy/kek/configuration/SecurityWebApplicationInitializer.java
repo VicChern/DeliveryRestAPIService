3b8e69e644bf3d7a968704c7cdcf6403f923c0e3
Olexiy Lyaschenko <lyaschenko2021@gmail.com>
2020-01-29 14:14:33 +0200

    [20] Added WebSecurityConfig, SecurityWebApplicationInitializer, MvcWebApplicationInitializer, Auth0Controller, but SpringSecurity doesn't work with Embedded Tomcat, also added spring.security.config, spring.security.web dependencies

---
+++ src/main/java/com/softserve/itacademy/kek/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.softserve.itacademy.kek.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
