36b6624c3c77b64b85cfe91b01e68064a50f31db
Olexandr Yermolenko <saaho.rmolenko@gmail.com>
2020-01-29 17:00:21 +0200

    Summary: Create a basic CRUD Repository for every entity Issue: link: https://gitlab.com/kv.061.java/project_kv_061/issues/3 Changes: Added classes and repositories for entities (Address, GlobalProperties, Identity, IdentityType, PropertyType, Tenant, TenantDetails, TenantProperties, User and UserDetails)

---
+++ src/main/java/com/softserve/itacademy/kek/models/Tenant.java
@@ -0,0 +1,102 @@
+package com.softserve.itacademy.kek.models;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "obj_tenant")
+public class Tenant implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idTenant;
+
+    @OneToOne
+    @JoinColumn(name ="id_tenant_owner", insertable = false, updatable = false)
+    private User tenantOwner;
+
+    @Column(name = "guid", nullable = false, unique = true)
+    private UUID guid;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToOne(mappedBy = "tenant")
+    private TenantDetails tenantDetails;
+
+    @OneToMany(mappedBy = "tenant")
+    private List<TenantProperties> tenantPropertiesList;
+
+    @OneToMany(mappedBy = "tenant")
+    private List<Address> addressList;
+
+    public int getIdTenant() {
+        return idTenant;
+    }
+
+    public void setIdTenant(int idTenant) {
+        this.idTenant = idTenant;
+    }
+
+    public UUID getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String UUID) {
+        this.guid = guid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getTenantOwner() {
+        return tenantOwner;
+    }
+
+    public void setTenantOwner(User tenantOwner) {
+        this.tenantOwner = tenantOwner;
+    }
+
+    public void setGuid(UUID guid) {
+        this.guid = guid;
+    }
+
+    public TenantDetails getTenantDetails() {
+        return tenantDetails;
+    }
+
+    public void setTenantDetails(TenantDetails tenantDetails) {
+        this.tenantDetails = tenantDetails;
+    }
+
+    public List<TenantProperties> getTenantPropertiesList() {
+        return tenantPropertiesList;
+    }
+
+    public void setTenantPropertiesList(List<TenantProperties> tenantPropertiesList) {
+        this.tenantPropertiesList = tenantPropertiesList;
+    }
+
+    public List<Address> getAddressList() {
+        return addressList;
+    }
+
+    public void setAddressList(List<Address> addressList) {
+        this.addressList = addressList;
+    }
+}
