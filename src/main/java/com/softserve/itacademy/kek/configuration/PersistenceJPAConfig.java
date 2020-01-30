51f5815b06a9ba110c35d6fbc85fce12ec4107c4
Galya Klymenko <gklymenko@gmail.com>
2020-01-30 07:30:02 +0200

    Summary: Create a JPA Config and basic IT test Issue link: https://gitlab.com/kv.061.java/project_kv_061/issues/3
    Changes:
    - Added dependency to support spring jpa repositories
    - Added PersistenceJPAConfig
    - Added UserRepositoryIt to check PersistenceJPAConfig works properly
    - Made minor changes to get User JPA repository works

---
+++ src/main/java/com/softserve/itacademy/kek/configuration/PersistenceJPAConfig.java
@@ -0,0 +1,68 @@
+package com.softserve.itacademy.kek.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+//TODO Extract db properties into separate files
+//TODO: Add logger
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"com.softserve.itacademy.kek.repositories"})
+public class PersistenceJPAConfig {
+
+    public PersistenceJPAConfig() {
+        super();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] { "com.softserve.itacademy.kek.models" });
+
+        final JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    final Properties additionalProperties() {
+        final Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        hibernateProperties.setProperty("hibernate.cache.use_second_level_cache", "false");
+
+        return hibernateProperties;
+    }
+
+    @Bean
+    JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+}
