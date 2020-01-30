a9d028aae059afcff4b665f3f6f6dbe35a38b107
Olexandr Yermolenko <saaho.rmolenko@gmail.com>
2020-01-30 18:28:13 +0200

    Summary: Added new entities and repositories Issue link: https://gitlab.com/kv.061.java/project_kv_061/issues/3 Changes: Added entities : Actor, ActorRole, Order, OrderDetails, OrderEvent, OrderEventType and repositories for them Changes datatype of id to Long

---
+++ src/main/java/com/softserve/itacademy/kek/models/Order.java
@@ -0,0 +1,90 @@
+package com.softserve.itacademy.kek.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "obj_order")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order")
+    private Long idOrder;
+
+    @ManyToOne
+    @JoinColumn(name ="id_tenant", insertable = false, updatable = false)
+    private Tenant idTenant;
+
+    @Column(name = "guid", unique = true, nullable = false)
+    private UUID guid;
+
+    @Size(min = 1, max = 256)
+    @Column(name = "summary", nullable = false)
+    private String summary;
+
+    @OneToOne(mappedBy = "idOrder")
+    private OrderDetails orderDetails;
+
+    @OneToMany(mappedBy = "idOrder")
+    private OrderEvent orderEvent;
+
+    public Long getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(Long idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public Tenant getIdTenant() {
+        return idTenant;
+    }
+
+    public void setIdTenant(Tenant idTenant) {
+        this.idTenant = idTenant;
+    }
+
+    public UUID getGuid() {
+        return guid;
+    }
+
+    public void setGuid(UUID guid) {
+        this.guid = guid;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public OrderDetails getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(OrderDetails orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    public OrderEvent getOrderEvent() {
+        return orderEvent;
+    }
+
+    public void setOrderEvent(OrderEvent orderEvent) {
+        this.orderEvent = orderEvent;
+    }
+}
--- src/main/java/com/softserve/itacademy/kek/repositories/TenantRepository.java
+++ src/main/java/com/softserve/itacademy/kek/repositories/TenantRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TenantRepository extends CrudRepository<Tenant, Integer> {
+public interface TenantRepository extends CrudRepository<Tenant, Long> {
 }
