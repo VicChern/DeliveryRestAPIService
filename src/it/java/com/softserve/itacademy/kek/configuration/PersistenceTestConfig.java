996dde141f3e9fb975f91b6f79a0b620a38b1762
Galya Klymenko <gklymenko@gmail.com>
2020-01-31 16:07:39 +0200

    Summary: Changed Spring provided DataSource into Tomcat Connection Pool Issue link: https://gitlab.com/kv.061.java/project_kv_061/issues/3
    Spring DriverManagerDataSource creates new Connection on every call so was changed to Tomcat Connection Pool

--- src/it/java/com/softserve/itacademy/kek/configuration/PersistenceTestConfig.java
+++ src/it/java/com/softserve/itacademy/kek/configuration/PersistenceTestConfig.java
@@ -1,12 +1,12 @@
 package com.softserve.itacademy.kek.configuration;
 
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -37,12 +37,13 @@ public class PersistenceTestConfig {
         em.setJpaVendorAdapter(vendorAdapter);
         vendorAdapter.setShowSql(true);
         em.setJpaProperties(additionalProperties(env));
+
         return em;
     }
 
     @Bean
     public DataSource dataSource(Environment env) {
-        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
         dataSource.setUrl(env.getProperty("jdbc.url"));
         dataSource.setUsername(env.getProperty("jdbc.user"));
