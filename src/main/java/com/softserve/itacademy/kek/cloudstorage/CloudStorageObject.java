c7948d998a8ced2b031af618c3802266a443d7f8
Valentyna Pivnytska <pivnytska@gmail.com>
2020-02-02 18:02:44 +0200

    [18] - Created a model entity

---
+++ src/main/java/com/softserve/itacademy/kek/cloudstorage/CloudStorageObject.java
@@ -0,0 +1,49 @@
+package com.softserve.itacademy.kek.cloudstorage;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class CloudStorageObject implements ICloudStorageObject{
+    String url;
+    String guid;
+    byte[] data;
+
+    public CloudStorageObject() {
+    }
+
+    public CloudStorageObject(String url, String guid, byte[] data) {
+        this.url = url;
+        this.guid = guid;
+        this.data = data;
+    }
+
+    @Override
+    public String getUrlString() { return url; }
+
+    @Override
+    public String getGuid() {
+        return guid;
+    }
+
+    @Override
+    public byte[] getDataBytes() {
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CloudStorageObject that = (CloudStorageObject) o;
+        return Objects.equals(url, that.url) &&
+                Objects.equals(guid, that.guid) &&
+                Arrays.equals(data, that.data);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(url, guid);
+        result = 31 * result + Arrays.hashCode(data);
+        return result;
+    }
+}
