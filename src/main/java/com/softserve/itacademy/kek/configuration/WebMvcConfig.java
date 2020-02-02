8674366f0aa4b0d9c92bdab64f668e3daadc5cf5
Olexiy Lyaschenko <lyaschenko2021@gmail.com>
2020-02-03 01:45:42 +0200

    #20 created auth0 authentication, added LoginController, LogoutController, CallbackController, ProfileInfoController. Only authenticated users have access to ProfileInfoController, it returns user info. Added TokenUtils, TokenAuthentication classes, changed WebSecurityConfig and moved it to 'security' folder

--- src/main/java/com/softserve/itacademy/kek/configuration/WebMvcConfig.java
+++ src/main/java/com/softserve/itacademy/kek/configuration/WebMvcConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
---
+++ src/main/java/com/softserve/itacademy/kek/security/WebSecurityConfig.java
@@ -0,0 +1,83 @@
+package com.softserve.itacademy.kek.security;
+
+import com.softserve.itacademy.kek.controller.LogoutController;
+
+import com.auth0.AuthenticationController;
+import com.auth0.jwk.JwkProvider;
+import com.auth0.jwk.JwkProviderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@PropertySource("classpath:auth0.properties")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * This is your auth0 domain (tenant you have created when registering with auth0 - account name)
+     */
+    @Value(value = "${com.auth0.domain}")
+    private String domain;
+
+    /**
+     * This is the client id of auth0 application
+     */
+    @Value(value = "${com.auth0.clientId}")
+    private String clientId;
+
+    /**
+     * This is the client secret of auth0 application
+     */
+    private final String clientSecret = System.getenv("clientSecret");
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        return new LogoutController();
+    }
+
+    @Bean
+    public AuthenticationController authenticationController() {
+        JwkProvider jwkProvider = new JwkProviderBuilder(domain).build();
+        return AuthenticationController.newBuilder(domain, clientId, clientSecret)
+                .withJwkProvider(jwkProvider)
+                .build();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http
+                .authorizeRequests()
+                .antMatchers("/profile/**")
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .successForwardUrl("/profile")
+                .and()
+                .logout()
+                .logoutSuccessHandler(logoutSuccessHandler())
+                .permitAll()
+        ;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+}
