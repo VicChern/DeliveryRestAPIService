4725d92ade170e1a07b3dabe95efb12e5b029e49
Valentyna Pivnytska <pivnytska@gmail.com>
2020-02-03 23:01:30 +0200

    [18] - Fixed code style, changed CloudStorageServiceException class

--- src/main/java/com/softserve/itacademy/kek/cloudstorage/ICloudStorageService.java
+++ src/main/java/com/softserve/itacademy/kek/cloudstorage/ICloudStorageService.java
@@ -1,9 +1,13 @@
 package com.softserve.itacademy.kek.cloudstorage;
 
+import com.softserve.itacademy.kek.exception.CloudStorageServiceException;
+
 import java.util.List;
 
 public interface ICloudStorageService {
     CloudStorageObject uploadBinaryData(byte[] data) throws CloudStorageServiceException;
+
     CloudStorageObject getCloudStorageObject(String guid) throws CloudStorageServiceException;
+
     List<CloudStorageObject> getCloudStorageObjects(String filter) throws CloudStorageServiceException;
 }
