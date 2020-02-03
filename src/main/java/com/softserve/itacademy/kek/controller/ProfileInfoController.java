8f69c768be48cdcaa1c0119e2ea0b77ca2f66e92
Olexiy Lyaschenko <lyaschenko2021@gmail.com>
2020-02-03 11:32:39 +0200

    deleted unused imports from ProfileInfoController and fixed WebSecurityConfig(deleted '/profile/**' from authenticaated urls

--- src/main/java/com/softserve/itacademy/kek/controller/ProfileInfoController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/ProfileInfoController.java
@@ -4,8 +4,6 @@ import com.softserve.itacademy.kek.security.TokenAuthentication;
 import com.softserve.itacademy.kek.security.TokenUtils;
 
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
