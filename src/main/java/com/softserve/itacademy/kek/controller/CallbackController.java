1803451ba8bae8ae49d532ebbc788cafec05b01f
Olexiy Lyaschenko <lyaschenko2021@gmail.com>
2020-02-03 17:47:09 +0200

    removed hardcoded code, removed clientSecret from auth0.properties

--- src/main/java/com/softserve/itacademy/kek/controller/CallbackController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/CallbackController.java
@@ -9,6 +9,8 @@ import com.auth0.jwt.JWT;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,20 +22,18 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping(path = "/callback")
+@PropertySource("classpath:server.properties")
 public class CallbackController extends DefaultController{
 
     @Autowired
     private AuthenticationController controller;
 
-    private final String redirectOnFail;
-    private final String redirectOnSuccess;
+    @Value(value = "${redirect.on.fail}")
+    private String redirectOnFail;
+    @Value(value = "${redirect.on.success}")
+    private String redirectOnSuccess;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    public CallbackController() {
-        this.redirectOnFail = "/login";
-        this.redirectOnSuccess = "/profile";
-    }
 
     @RequestMapping
     protected void getCallback(HttpServletRequest req, HttpServletResponse res) throws IOException {
