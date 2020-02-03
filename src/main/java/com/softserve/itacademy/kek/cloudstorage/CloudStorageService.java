38313a3944bfd0c31e08e7c423b4151e490fec72
Valentyna Pivnytska <pivnytska@gmail.com>
2020-02-03 23:20:03 +0200

    Changed urls, removed the duplicate code, fixed hardcoding values

--- src/main/java/com/softserve/itacademy/kek/cloudstorage/CloudStorageService.java
+++ src/main/java/com/softserve/itacademy/kek/cloudstorage/CloudStorageService.java
@@ -1,11 +1,5 @@
 package com.softserve.itacademy.kek.cloudstorage;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 import com.google.api.gax.paging.Page;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Blob;
@@ -13,57 +7,60 @@ import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.softserve.itacademy.kek.exception.CloudStorageServiceException;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 
 public class CloudStorageService implements ICloudStorageService {
+
     @Override
     public CloudStorageObject uploadBinaryData(byte[] data) throws CloudStorageServiceException {
-
-        Storage storage = getStorageObject();
-        Bucket bucket = storage.update(BucketInfo.of("kek-default-bucket"));
-
-        String guid = UUID.randomUUID().toString();
-        Blob blob = bucket.create(guid, data);
-
-        String url = "https://storage.cloud.google.com/" + bucket.getName() + "/" + blob.getName() + "?authuser=1";
-        return new CloudStorageObject(url, guid, data);
+        String bucket = System.getenv("BUCKET_NAME");
+        return uploadBinaryData(data, bucket);
     }
 
     public CloudStorageObject uploadBinaryData(byte[] data, String bucketName) throws CloudStorageServiceException {
-
         Storage storage = getStorageObject();
         Bucket bucket = null;
 
-        if (storage.get(bucketName) != null){
+        if (storage.get(bucketName) != null) {
             bucket = storage.update(BucketInfo.of(bucketName));
         } else {
             bucket = storage.create(BucketInfo.of(bucketName));
         }
-
         String guid = UUID.randomUUID().toString();
         Blob blob = bucket.create(guid, data);
-
-        String url = "https://storage.cloud.google.com/" + bucket.getName() + "/" + blob.getName() + "?authuser=1";
+        String url = blob.getMediaLink();
         return new CloudStorageObject(url, guid, data);
     }
 
     @Override
     public CloudStorageObject getCloudStorageObject(String guid) throws CloudStorageServiceException {
-        Storage storage = getStorageObject();
-        Bucket bucket = storage.update(BucketInfo.of("kek-default-bucket"));
-        Blob blob = bucket.get(guid);
-        byte[] data = blob.getContent();
-        String url = "https://storage.cloud.google.com/" + bucket.getName() + "/" + guid + "?authuser=1";
-        return new CloudStorageObject(url, guid, data);
+        String bucket = System.getenv("BUCKET_NAME");
+        return getCloudStorageObject(guid, bucket);
     }
 
     public CloudStorageObject getCloudStorageObject(String guid, String bucketName) throws CloudStorageServiceException {
         Storage storage = getStorageObject();
-        Bucket bucket = storage.update(BucketInfo.of(bucketName));
-        Blob blob = bucket.get(guid);
-        byte[] data = blob.getContent();
-        String url = "https://storage.cloud.google.com/" + bucket.getName() + "/" + guid + "?authuser=1";
-        return new CloudStorageObject(url, guid, data);
+
+        if (storage.get(bucketName) != null) {
+            return null;
+        } else {
+            Bucket bucket = storage.update(BucketInfo.of(bucketName));
+            Blob blob = bucket.get(guid);
+            byte[] data = blob.getContent();
+            String url = blob.getMediaLink();
+            return new CloudStorageObject(url, guid, data);
+        }
     }
 
     @Override
@@ -75,8 +72,8 @@ public class CloudStorageService implements ICloudStorageService {
 
         Page<Blob> blobs = bucket.list();
 
-        for (Blob blob : blobs.getValues()){
-            String url = "https://storage.cloud.google.com/" + filter + "/" + blob.getName() + "?authuser=1";
+        for (Blob blob : blobs.getValues()) {
+            String url = blob.getMediaLink();
             String guid = blob.getName();
             byte[] data = blob.getContent();
             objects.add(new CloudStorageObject(url, guid, data));
@@ -89,12 +86,18 @@ public class CloudStorageService implements ICloudStorageService {
         StorageOptions storageOptions = null;
         String token = System.getenv("GOOGLE_CLOUD_STORAGE_KEY");
         try {
+            Object obj = new JSONParser().parse(new FileReader(token)); //parses token and gets projectID
+            JSONObject jo = (JSONObject) obj;
+            String projectID = (String) jo.get("project_id");
+
             storageOptions = StorageOptions.newBuilder()
-                    .setProjectId("kinda-express-king-266805")
+                    .setProjectId(projectID)
                     .setCredentials(GoogleCredentials.fromStream(new FileInputStream(token)))
                     .build();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new CloudStorageServiceException("Token was not found");
+        } catch (ParseException e) {
+            throw new CloudStorageServiceException("Token was not parsed");
         }
 
         Storage storage = storageOptions.getService();
