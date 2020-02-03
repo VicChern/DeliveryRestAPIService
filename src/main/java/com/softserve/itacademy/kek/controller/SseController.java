46397c9462d54c7db7971c1b66d65ce307027ed3
MarynaKontar <marina.acoustic@gmail.com>
2020-02-03 17:50:31 +0200

    issue-21-Realtime Tracking Controller (HTTP SSE) Summary: Adds frontend part to log to console response for realtime tracking Issue link: https://gitlab.com/kv.061.java/project_kv_061/issues/21 Changes: 1. Adds frontend component to log to console response from /api/v1/request 2. change endpoint /api/v1/request to give ResponseEntity<ResponseBodyEmitter>

--- src/main/java/com/softserve/itacademy/kek/controller/SseController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/SseController.java
@@ -1,19 +1,24 @@
 package com.softserve.itacademy.kek.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 // how long to wait with opened connection?
 // do we need fixed timeOut
 
-@Controller
+@RestController
 public class SseController {
     private int timeOut = 1000;
     private boolean isAlive = true;
@@ -51,30 +56,51 @@ public class SseController {
         }
     }
 
+//    @GetMapping("/request")
+//    @Async
+//    public SseEmitter handleRequest() {
+//        SseEmitter emitter = new SseEmitter();
+//
+//
+//        ExecutorService service = Executors.newSingleThreadExecutor();
+//        service.execute(() ->  {
+//            try {
+//                for ( int i = 0 ; isAlive ; i++ ) {
+//                    SseEmitter.SseEventBuilder event = SseEmitter.event()
+//                            .id(String.valueOf(i))
+//                            .name("name")
+//                            .data("message " + i, MediaType.TEXT_PLAIN);
+//                    emitter.send(event);
+//                    Thread.sleep(timeOut);
+//                }
+//            } catch (Exception e ) {
+//                e.printStackTrace();
+//                emitter.completeWithError(e);
+//            }
+//        });
+//
+//        return emitter;
+//    }
+//
+
+
     @GetMapping("/request")
     @Async
-    public SseEmitter handleRequest() {
-        SseEmitter emitter = new SseEmitter();
-
-
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        service.execute(() ->  {
+    public ResponseEntity<ResponseBodyEmitter> handleRbe() {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+        executor.execute(() -> {
             try {
-                for ( int i = 0 ; isAlive ; i++ ) {
-                    SseEmitter.SseEventBuilder event = SseEmitter.event()
-                            .id(String.valueOf(i))
-                            .name("name")
-                            .data("message " + i, MediaType.TEXT_PLAIN);
-                    emitter.send(event);
-                    Thread.sleep(timeOut);
-                }
-            } catch (Exception e ) {
-                e.printStackTrace();
-                emitter.completeWithError(e);
+                emitter.send(
+                        "/request" + " @ " + new Date(), MediaType.TEXT_PLAIN);
+                emitter.complete();
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
             }
         });
-
-        return emitter;
+        return new ResponseEntity(emitter, HttpStatus.OK);
     }
+
+
 }
 
