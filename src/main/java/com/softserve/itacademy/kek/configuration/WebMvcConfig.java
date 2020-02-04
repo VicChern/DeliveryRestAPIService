d3bf6ed5487dda3eaec68d690bd20b3e2a41021a
vicchern <itstud87@gmail.com>
2020-02-04 20:42:34 +0200

    Summary: Minor refactoring of code in the WebMvcConfig.class
    org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter was depricated.
    This why class WebMvcConfig will implements WebMvcConfigurer interface instead WebMvcConfigurerAdapter.class

--- src/main/java/com/softserve/itacademy/kek/configuration/WebMvcConfig.java
+++ src/main/java/com/softserve/itacademy/kek/configuration/WebMvcConfig.java
@@ -29,5 +29,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         final Map<String, String> parameterMap = new HashMap<>();
         parameterMap.put("charset", "UTF-8");
         configurer.defaultContentType(new MediaType(MediaType.APPLICATION_JSON, parameterMap));
+
     }
 }
