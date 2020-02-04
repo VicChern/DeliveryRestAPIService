6abd86551e7e84f55988e45de559d0ea862f56e0
Roman Kravchenko <kravchenko-rn@ukr.net>
2020-02-04 08:46:08 +0200

    Removed unnecessary import; Changed the name of the method related to HTTP GET

--- src/main/java/com/softserve/itacademy/kek/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/UserController.java
@@ -3,7 +3,6 @@ package com.softserve.itacademy.kek.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -93,7 +92,7 @@ public class UserController extends DefaultController {
     }
 
     @GetMapping("/{id}/address/{addrguid}")
-    public ResponseEntity<String> findUserAddress(@PathVariable String id, @PathVariable String addrGuid) {
+    public ResponseEntity<String> getUserAddress(@PathVariable String id, @PathVariable String addrGuid) {
         return ResponseEntity.ok(getJSON(id, "15v, Leipzigzskaya st, Kiev"));
     }
 
