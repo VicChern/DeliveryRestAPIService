f30022459edc5aef2c9e9df04d7e74a66fa234b8
vicchern <itstud87@gmail.com>
2020-02-04 17:52:09 +0200

    Added minor changes to method signatures

--- src/main/java/com/softserve/itacademy/kek/configuration/WebMvcConfig.java
+++ src/main/java/com/softserve/itacademy/kek/configuration/WebMvcConfig.java
@@ -9,11 +9,11 @@ import org.springframework.web.servlet.config.annotation.ContentNegotiationConfi
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
