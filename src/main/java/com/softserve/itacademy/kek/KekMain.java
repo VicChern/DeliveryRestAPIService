6432e8c3f06ce0625fd165fb407969ae6440011e
Roman Kravchenko <kravchenko-rn@ukr.net>
2020-02-06 09:50:29 +0200

    Added logging to the main class, the user controller and the default controller

--- src/main/java/com/softserve/itacademy/kek/KekMain.java
+++ src/main/java/com/softserve/itacademy/kek/KekMain.java
@@ -2,12 +2,18 @@ package com.softserve.itacademy.kek;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+
 public class KekMain {
+    final static Logger logger = Logger.getLogger(KekMain.class);
 
     public static void main(String[] args) {
         try {
+            logger.info("Starting embedded tomcat");
             new EmbeddedTomcatApp().start();
+            logger.info("Embedded tomcat started");
         } catch (IOException e) {
+            logger.error("Could not start embedded tomcat");
             e.printStackTrace();
         }
     }
--- src/main/java/com/softserve/itacademy/kek/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.softserve.itacademy.kek.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.JSONObject;
 
-// TODO: Add logger
-
 @RestController
 @RequestMapping(path = "/users", produces = "application/json; charset=UTF-8")
 public class UserController extends DefaultController {
+    final Logger logger = Logger.getLogger(UserController.class);
 
     // Build Response (stub, temporary method)
     private String getJSON(String id, String status) {
@@ -32,9 +32,13 @@ public class UserController extends DefaultController {
      */
     @GetMapping
     public ResponseEntity<String> getUserList() {
+        logger.info("Client requested the list of all users");
+
         JSONObject json = new JSONObject();
         json.append("userID", "1").append("userID", "2").append("userID", "3");
         json.put("status", "received");
+
+        logger.info("Sending list of all users to the client");
         return ResponseEntity.ok(json.toString());
     }
 
@@ -46,6 +50,7 @@ public class UserController extends DefaultController {
      */
     @GetMapping("/{id}")
     public ResponseEntity<String> getUser(@PathVariable String id) {
+        logger.info("Sending the user(" + id + ") to the client");
         return ResponseEntity.ok(getJSON(id, "received"));
     }
 
@@ -57,18 +62,20 @@ public class UserController extends DefaultController {
      */
     @PostMapping
     public ResponseEntity<String> addUser(@RequestBody String body) {
+        logger.info("Sending the created user to the client");
         return ResponseEntity.ok(body);
     }
 
     /**
      * Modifies information of the specified user
      *
-     * @param id user ID from the URN
+     * @param id   user ID from the URN
      * @param body user object as a JSON
      * @return modified user object as a JSON
      */
     @PutMapping("/{id}")
     public ResponseEntity<String> modifyUser(@PathVariable String id, @RequestBody String body) {
+        logger.info("Sending the modified user to the client");
         return ResponseEntity.ok(body);
     }
 
@@ -80,6 +87,7 @@ public class UserController extends DefaultController {
      */
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable String id) {
+        logger.info("User (" + id + ") successfully deleted");
         return ResponseEntity.ok(getJSON(id, "deleted"));
     }
 
@@ -91,30 +99,33 @@ public class UserController extends DefaultController {
      */
     @GetMapping("/{id}/addresses")
     public ResponseEntity<String> getUserAddresses(@PathVariable String id) {
+        logger.info("Sending the list of user(" + id + ") addresses to the client");
         return ResponseEntity.ok(getJSON(id, "received"));
     }
 
     /**
      * Adds a new addresses for the specific user
      *
-     * @param id user ID from the URN
+     * @param id   user ID from the URN
      * @param body list of address objects as a JSON
      * @return list of the created address objects as a JSON
      */
     @PostMapping("/{id}/addresses")
     public ResponseEntity<String> addUserAddresses(@PathVariable String id, @RequestBody String body) {
+        logger.info("Sending the created user(" + id + ") addresses to the client");
         return ResponseEntity.ok(body);
     }
 
     /**
      * Finds addresses of the specific user
      *
-     * @param id user ID from the URN
+     * @param id       user ID from the URN
      * @param addrGuid address ID from the URN
      * @return address object as a JSON
      */
     @GetMapping("/{id}/addresses/{addrguid}")
     public ResponseEntity<String> getUserAddress(@PathVariable("id") String id, @PathVariable("addrguid") String addrGuid) {
+        logger.info("Sending the user(" + id + ") address(" + addrGuid + ") to the client");
         return ResponseEntity.ok(getJSON(id, "received"));
     }
 
@@ -122,25 +133,27 @@ public class UserController extends DefaultController {
     /**
      * Modifies the specific user address tenant property
      *
-     * @param id user ID from the URN
+     * @param id       user ID from the URN
      * @param addrGuid address ID from the URN
-     * @param body address object as a JSON
+     * @param body     address object as a JSON
      * @return modified address object as a JSON
      */
     @PutMapping("/{id}/addresses/{addrguid}")
     public ResponseEntity<String> modifyUserAddress(@PathVariable("id") String id, @PathVariable("addrguid") String addrGuid, @RequestBody String body) {
+        logger.info("Sending the modified user(" + id + ") address(" + addrGuid + ") to the client");
         return ResponseEntity.ok(body);
     }
 
     /**
      * Deletes the specific user address
      *
-     * @param id user ID from the URN
+     * @param id       user ID from the URN
      * @param addrGuid address ID from the URN
      * @return operation status as a JSON
      */
     @DeleteMapping("/{id}/addresses/{addrguid}")
     public ResponseEntity<String> deleteUserAddress(@PathVariable("id") String id, @PathVariable("addrguid") String addrGuid) {
+        logger.info("User (" + id + ") address(" + addrGuid + ") successfully deleted");
         return ResponseEntity.ok(getJSON(id, "deleted"));
     }
 }
