cf9ce0b962b0acb5b52d715432873ec6f1337f0e
Roman Kravchenko <kravchenko-rn@ukr.net>
2020-02-07 11:42:57 +0200

    i24. Added DTO classes

---
+++ src/main/java/com/softserve/itacademy/kek/dto/UserDto.java
@@ -0,0 +1,77 @@
+package com.softserve.itacademy.kek.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Objects;
+
+public class UserDto {
+    private String guid;
+    private String name;
+    private String nickname;
+    private String email;
+    private String phone;
+    private DetailsDto details;
+
+    public UserDto(String guid, String name, String nickName, String email, String phone, DetailsDto detailsDto) {
+        this.guid = guid;
+        this.name = name;
+        this.nickname = nickName;
+        this.email = email;
+        this.phone = phone;
+        this.details = detailsDto;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public DetailsDto getDetails() {
+        return details;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserDto)) return false;
+        UserDto userDto = (UserDto) o;
+        return Objects.equals(guid, userDto.guid) &&
+                Objects.equals(name, userDto.name) &&
+                Objects.equals(nickname, userDto.nickname) &&
+                Objects.equals(email, userDto.email) &&
+                Objects.equals(phone, userDto.phone) &&
+                Objects.equals(details, userDto.details);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(guid, name, nickname, email, phone, details);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "guid='" + guid + '\'' +
+                ", name='" + name + '\'' +
+                ", nickName='" + nickname + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", detailsDto=" + details +
+                '}';
+    }
+}
