aad9ff6fc047dbe563b005369187aa5447dd0d0c
Olexiy Lyaschenko <lyaschenko2021@gmail.com>
2020-02-08 21:33:15 +0200

    Summary: Wrote integration test for WebSecurityConfig Issue link: https://gitlab.com/kv.061.java/project_kv_061/issues/20 Changes: Added WebSecurityIntegrationTest, dependency spring-security-test in pom.xml and file with integration-test properties

---
+++ src/it/java/com/softserve/itacademy/kek/security/WebSecurityIntegrationTest.java
@@ -0,0 +1,56 @@
+package com.softserve.itacademy.kek.security;
+
+import com.softserve.itacademy.kek.configuration.WebMvcConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Test(groups = {"integration-tests"})
+@ContextConfiguration(classes = {WebMvcConfig.class, WebSecurityConfig.class})
+@WebAppConfiguration
+@Configuration
+@TestPropertySource(locations = "classpath:integration-test.properties")
+public class WebSecurityIntegrationTest extends AbstractTestNGSpringContextTests {
+
+    @Value(value = "${redirect.URL.when.not.authenticated}")
+    private String redirectUrlWhenNotAuthenticated;
+
+    @Value(value = "requested.page.URL")
+    private String requestedPageURL;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeMethod
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void routeRedirectWhenNotAuthenticated() throws Exception {
+        mvc.perform(get(requestedPageURL))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(redirectUrlWhenNotAuthenticated));
+    }
+
+
+}
