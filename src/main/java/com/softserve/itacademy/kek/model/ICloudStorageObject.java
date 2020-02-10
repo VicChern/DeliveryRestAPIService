dc2584f96e3b3a5de3ecd019d880b1524dcd85a1
Valentyna Pivnytska <pivnytska@gmail.com>
2020-02-10 18:54:38 +0200

    [18] - Added javadoc comments

--- src/main/java/com/softserve/itacademy/kek/model/ICloudStorageObject.java
+++ src/main/java/com/softserve/itacademy/kek/model/ICloudStorageObject.java
@@ -2,9 +2,24 @@ package com.softserve.itacademy.kek.model;
 
 public interface ICloudStorageObject {
 
+    /**
+     * Returns URL of the specific Google Cloud Storage object
+     *
+     * @return URL
+     */
     String getUrlString();
 
+    /**
+     * Returns GUID of the specific Google Cloud Storage object
+     *
+     * @return unique GUID
+     */
     String getGuid();
 
+    /**
+     * Returns data as byte array of the specific Google Cloud storage object
+     *
+     * @return byte array
+     */
     byte[] getDataBytes();
 }
