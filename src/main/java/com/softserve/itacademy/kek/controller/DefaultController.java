fac9ddbdd9cda8e7c912f6eb43c90d747f616fed
MarynaKontar <marina.acoustic@gmail.com>
2020-02-11 10:48:29 +0200

    Summary: Create skeleton of service flow for Tenant Issue link: #26 Changes: 1. added method to ITenantService according to Public Rest API; 2. created skeleton of object layer for decoupling service module and controller modules; 3. created TenantObject; 4. created converter to transform TenantObject to Tenant and vice versa; 5. created ServiceException, BadRequestException and TenantServiceException for wrapping exceptions of service layer; 5. implemented method save(TenantObject tenantObject); 6. added exception handling for ServiceException to DefaultController.

--- src/main/java/com/softserve/itacademy/kek/controller/DefaultController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/DefaultController.java
@@ -17,6 +17,23 @@ import javax.servlet.http.HttpServletRequest;
 public class DefaultController {
     final Logger logger = LoggerFactory.getLogger(DefaultController.class);
 
+
+    /**
+     * Handles {@link ServiceException} which were not handled locally
+     *
+     * @param ex the exception which occurred in services
+     * @return the error message as a JSON
+     */
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<String> serviceExceptionHandler(ServiceException ex) {
+        logger.error("An error occurred:", ex);
+        JSONObject response = new JSONObject()
+                .put("Error", "Message that we want to send to client about error with services");
+        logger.warn("Sending the error message to the client");
+        return ResponseEntity.ok(response.toString());
+    }
+
+
     /**
      * ServiceException handler.
      * @param ex ServiceException for handling.
