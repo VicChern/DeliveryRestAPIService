9b7dfd276244c793403cdc2d9222b15cd5f0ac99
Ihor Zahoruiko <igorvladz@gmail.com>
2020-02-13 00:17:08 +0200

    [26] Added Service for User

---
+++ src/main/java/com/softserve/itacademy/kek/modelInterfaces/IUserData.java
@@ -0,0 +1,43 @@
+package com.softserve.itacademy.kek.modelInterfaces;
+
+import java.util.UUID;
+
+/**
+ * Interface for User data exchange with Service Layer
+ */
+public interface IUserData {
+    /**
+     * Returns user GUID
+     *
+     * @return user GUID
+     */
+    UUID getGuid();
+
+    /**
+     * Returns user name
+     *
+     * @return user name
+     */
+    String getName();
+
+    /**
+     * Returns user nickname
+     *
+     * @return user nickname
+     */
+    String getNickname();
+
+    /**
+     * Returns user email
+     *
+     * @return user email
+     */
+    String getEmail();
+
+    /**
+     * Returns user phone number
+     *
+     * @return user phone number
+     */
+    String getPhoneNumber();
+}
