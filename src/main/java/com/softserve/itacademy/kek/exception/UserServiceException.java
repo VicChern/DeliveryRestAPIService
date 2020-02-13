b06e3e853c0471ebe587ace7a3b8f300e30c3231
Ihor Zahoruiko <igorvladz@gmail.com>
2020-02-13 13:08:06 +0200

    [26] Fix comment (for javadoc)

--- src/main/java/com/softserve/itacademy/kek/exception/UserServiceException.java
+++ src/main/java/com/softserve/itacademy/kek/exception/UserServiceException.java
@@ -1,7 +1,9 @@
 package com.softserve.itacademy.kek.exception;
 
+import com.softserve.itacademy.kek.services.impl.UserServiceImpl;
+
 /**
- * Exception for {@UserServiceImpl}
+ * Exception for {@link UserServiceImpl}
  */
 public class UserServiceException extends BadRequestException {
     public UserServiceException(String message) {
