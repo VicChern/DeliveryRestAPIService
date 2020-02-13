6a691d921e3fe27b887d73d15986486bee0ea4c9
Olexandr Yermolenko <saaho.rmolenko@gmail.com>
2020-02-13 17:25:34 +0200

    Summary: Added service for Order Issue link: https://gitlab.com/kv.061.java/project_kv_061/issues/26 Changes: Added service for Order Cleared unneeded services

--- src/main/java/com/softserve/itacademy/kek/services/IOrderService.java
+++ src/main/java/com/softserve/itacademy/kek/services/IOrderService.java
@@ -1,4 +1,53 @@
 package com.softserve.itacademy.kek.services;
 
+import com.softserve.itacademy.kek.modelInterfaces.IOrder;
+import com.softserve.itacademy.kek.models.Order;
+
+import java.util.UUID;
+
+/**
+ * Service interface for {@link IOrder}
+ */
+
 public interface IOrderService {
+
+    /**
+     * Saved new {@link Order} to db
+     *
+     * @param order order
+     * @return saved order
+     */
+    IOrder save(IOrder order);
+
+    /**
+     * Gets all orders
+     *
+     * @return a list of all orders
+     */
+    Iterable<IOrder> getAll();
+
+    /**
+     * Gets order by {@link Order} guid
+     *
+     * @param guid {@link Order} guid
+     * @return guid
+     */
+    IOrder getByGuid(UUID guid);
+
+    /**
+     * Updates {@link Order}
+     *
+     * @param order
+     * @param guid  {@link Order} guid
+     * @return updated order
+     */
+    IOrder update(IOrder order, UUID guid);
+
+    /**
+     * Deletes order by {@link Order} guid
+     *
+     * @param guid
+     */
+    void deleteByGuid(UUID guid);
+
 }
