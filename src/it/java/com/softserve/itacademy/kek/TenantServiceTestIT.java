a85fce79b3e5490fa6a362e0ab1e9087ba2cecb6
MarynaKontar <marina.acoustic@gmail.com>
2020-02-16 19:52:49 +0200

    Summary: Added request information for logger in Exception handler for ServiceException; added ability for service layer integration testing; fix bug with deleting tenant Issue link: #26 1. added HttpServletRequest information for logger in Exception handler for ServiceException; 2. added ability for service layer integration testing by adding @ComponentScan to PersistenceTestConfig; 3. fix bug with deleting tenant by changing cascade = CascadeType.ALL to cascade = {CascadeType.REMOVE} for tenantOwner in User. !!!!! Need to change the TenantPropertiesTestIT because in these tests properties are created through user creation, but they must be created for an existing in db tenant (according to Public Rest API)

---
+++ src/it/java/com/softserve/itacademy/kek/TenantServiceTestIT.java
@@ -0,0 +1,52 @@
+package com.softserve.itacademy.kek;
+
+import com.softserve.itacademy.kek.configuration.PersistenceTestConfig;
+import com.softserve.itacademy.kek.modelInterfaces.IUser;
+import com.softserve.itacademy.kek.models.User;
+import com.softserve.itacademy.kek.repositories.TenantRepository;
+import com.softserve.itacademy.kek.repositories.UserRepository;
+import com.softserve.itacademy.kek.services.ITenantService;
+import com.softserve.itacademy.kek.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.softserve.itacademy.kek.utils.ITCreateEntitiesUtils.createOrdinaryUser;
+
+@Test(groups = {"integration-tests"})
+@ContextConfiguration(classes = {PersistenceTestConfig.class})
+public class TenantServiceTestIT extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private ITenantService tenantService;
+    @Autowired
+    private IUserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TenantRepository tenantRepository;
+
+    @BeforeMethod
+    public void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+//        userRepository.deleteAll();
+    }
+
+    @Rollback
+    @Test
+    public void createSuccess() {
+       IUser user = createOrdinaryUser(1);
+       IUser savedUser = userService.create(user);
+        System.out.println();
+    }
+
+}
