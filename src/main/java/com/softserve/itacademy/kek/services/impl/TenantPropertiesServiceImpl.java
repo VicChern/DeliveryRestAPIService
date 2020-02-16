47e9df304add6ebb7decb41182859466a48a2804
MarynaKontar <marina.acoustic@gmail.com>
2020-02-16 23:47:12 +0200

    Summary: Added skeleton for ability to implement service for TenantProperties Issue link: #26 Changes: 1. added guid field to TenantProperties according to Public-Rest-API; 2. added ITenantProperties, IPropertyType interfaces for data exchange with service layer; 3. added changes in TenantProperties and PropertyType for implement these interfaces; 4. added methods to ITenantPropertiesService and skeleton of these methods in TenantPropertiesServiceImpl according to Public-Rest-API; 5. added guid field to creating TenantProperties method in util class for integration tests. Tests: check all tests before commit.

--- src/main/java/com/softserve/itacademy/kek/services/impl/TenantPropertiesServiceImpl.java
+++ src/main/java/com/softserve/itacademy/kek/services/impl/TenantPropertiesServiceImpl.java
@@ -1,9 +1,51 @@
 package com.softserve.itacademy.kek.services.impl;
 
+import com.softserve.itacademy.kek.modelInterfaces.ITenantProperties;
+import com.softserve.itacademy.kek.models.TenantProperties;
 import com.softserve.itacademy.kek.services.ITenantPropertiesService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TenantPropertiesServiceImpl implements ITenantPropertiesService {
 
+    @Override
+    public List<ITenantProperties> getTenantPropertiesByTenantGuid(UUID tenantGuid) {
+        return null;
+    }
+
+    @Override
+    public List<ITenantProperties> createTenantPropertiesForTenant(List<ITenantProperties> tenantProperties, UUID tenantGuid) {
+        return null;
+    }
+
+    @Override
+    public ITenantProperties getTenantPropertyForTenantByGuid(UUID tenantGuid, UUID tenantPropertyGuid) {
+        return null;
+    }
+
+    @Override
+    public ITenantProperties updateTenantPropertyForTenantByGuid(UUID tenantGuid, UUID tenantPropertyGuid, ITenantProperties tenantProperty) {
+        return null;
+    }
+
+    @Override
+    public void deleteTenantPropertyForTenantByGuid(UUID tenantGuid, UUID tenantProperty) {
+
+    }
+
+    private TenantProperties transform(ITenantProperties iTenantProperties) {
+
+        TenantProperties tenantProperties = new TenantProperties();
+
+        tenantProperties.setGuid(iTenantProperties.getGuid());
+        tenantProperties.setKey(iTenantProperties.getKey());
+        tenantProperties.setValue(iTenantProperties.getValue());
+        tenantProperties.setPropertyType(iTenantProperties.getPropertyType());
+        tenantProperties.setTenant(iTenantProperties.getTenant());
+
+        return tenantProperties;
+    }
 }
