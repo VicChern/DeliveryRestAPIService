3684dc2e8d0f47341cfb11a1991ddfacc5dd3d24
Roman Kravchenko <kravchenko-rn@ukr.net>
2020-02-17 15:35:23 +0200

    i34 - Added dto validation handler

--- src/main/java/com/softserve/itacademy/kek/controller/DefaultController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/DefaultController.java
@@ -1,13 +1,18 @@
 package com.softserve.itacademy.kek.controller;
 
-import org.json.JSONObject;
+import java.util.LinkedList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.softserve.itacademy.kek.dto.ErrorsDto;
 
 @RestController
 public class DefaultController {
@@ -20,19 +25,63 @@ public class DefaultController {
      * @return the error message as a JSON
      */
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> defaultExceptionHandler(Exception ex) {
+    public ResponseEntity<ErrorsDto> defaultExceptionHandler(Exception ex) {
         logger.error("An error occurred:", ex);
-        JSONObject response = new JSONObject()
-                .put("Error", "Something went wrong...");
+        ErrorsDto errors = new ErrorsDto(new LinkedList<>());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/vnd.softserve.error+json");
+
+        errors.addError(ex.getMessage());
+
         logger.warn("Sending the error message to the client");
-        return ResponseEntity.ok(response.toString());
+        return ResponseEntity
+                .badRequest()
+                .headers(headers)
+                .body(errors);
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
-    public ResponseEntity<String> validationExceptionHandler(Exception ex) {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorsDto> validationExceptionHandler(HttpMessageNotReadableException ex) {
         logger.error("An error occurred:", ex);
+
+        ErrorsDto errors = new ErrorsDto(new LinkedList<>());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/vnd.softserve.error+json");
+
+        errors.addError(ex.getMessage());
+
         logger.warn("Sending the error message to the client");
-        return ResponseEntity.badRequest().body("Not valid request");
+        return ResponseEntity
+                .badRequest()
+                .headers(headers)
+                .body(errors);
     }
 
+    /**
+     * Handles the DTO validation exceptions
+     *
+     * @param ex caught exception
+     * @return a ResponseEntity instance with the {@link ErrorsDto} object
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorsDto> dtoValidationHandler(MethodArgumentNotValidException ex) {
+        logger.error("An error occurred:", ex);
+
+        ErrorsDto errors = new ErrorsDto(new LinkedList<>());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/vnd.softserve.error+json");
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errors.addError(fieldError.getDefaultMessage(), fieldError.getField());
+        }
+
+        logger.warn("Sending the error message to the client");
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(errors);
+    }
 }
