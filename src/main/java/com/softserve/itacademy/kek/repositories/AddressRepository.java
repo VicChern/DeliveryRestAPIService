57c198d35552bc9947d77e9d18ba627f14f32979
Ihor Zahoruiko <igorvladz@gmail.com>
2020-02-17 14:42:10 +0200

    [26] Address Service: added obtaining user address list (by user guid) and tenant address list (by tenant guid).

--- src/main/java/com/softserve/itacademy/kek/repositories/AddressRepository.java
+++ src/main/java/com/softserve/itacademy/kek/repositories/AddressRepository.java
@@ -1,13 +1,35 @@
 package com.softserve.itacademy.kek.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.softserve.itacademy.kek.models.Address;
 
-public interface AddressRepository extends CrudRepository<Address, Long> {
+public interface AddressRepository extends JpaRepository<Address, Long> {
 
+    /**
+     * Returns address by guid
+     *
+     * @param guid address guid
+     * @return address
+     */
     Address findByGuid(UUID guid);
 
+    /**
+     * Returns user address list
+     *
+     * @param guid user guid
+     * @return user address list
+     */
+    List<Address> findAllByUserGuid(UUID guid);
+
+    /**
+     * Returns tenant address list
+     *
+     * @param guid tenant guid
+     * @return tenant address list
+     */
+    List<Address> findAllByTenantGuid(UUID guid);
 }
