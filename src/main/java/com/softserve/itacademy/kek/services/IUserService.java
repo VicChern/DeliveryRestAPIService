d3651fdd82d6f3d2d568390ae38cf702c2dd3360
Ihor Zahoruiko <igorvladz@gmail.com>
2020-02-17 15:13:15 +0200

    [26] User Service: the method getAll has to return java.util.List

--- src/main/java/com/softserve/itacademy/kek/services/IUserService.java
+++ src/main/java/com/softserve/itacademy/kek/services/IUserService.java
@@ -1,5 +1,6 @@
 package com.softserve.itacademy.kek.services;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.softserve.itacademy.kek.dataexchange.IUser;
@@ -44,5 +45,5 @@ public interface IUserService {
      *
      * @return all users
      */
-    Iterable<IUser> getAll();
+    List<IUser> getAll();
 }
