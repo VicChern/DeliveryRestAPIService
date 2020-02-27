7810c312f68d4da81b088aa39387f5be1b1888fb
Olexandr Yermolenko <saaho.rmolenko@gmail.com>
2020-02-27 14:50:15 +0200

    Summary: Added getAllEventsForOrder method Issue link: https://gitlab.com/kv.061.java/project_kv_061/issues/52 Changes:  - Added new method in IOrderEventService  - Added getAllEventsForOrder method in OrderEventServiceImpl  - Added test for getAllEventsForOrder method  - Changed getOrderEvent method in ITCreateEntitiesUtils  - Changed final Logger logger to private final static Logger LOGGER

--- src/it/java/com/softserve/itacademy/kek/utils/ITCreateEntitiesUtils.java
+++ src/it/java/com/softserve/itacademy/kek/utils/ITCreateEntitiesUtils.java
@@ -263,10 +263,11 @@ public class ITCreateEntitiesUtils {
         return orderEventType;
     }
 
-    public static OrderEvent getOrderEvent(Order order, OrderEventType orderEventType) {
+    public static OrderEvent getOrderEvent(Order order, OrderEventType orderEventType, Actor actor) {
         OrderEvent orderEvent = new OrderEvent();
 
         orderEvent.setOrder(order);
+        orderEvent.setActor(actor);
         orderEvent.setOrderEventType(orderEventType);
         orderEvent.setGuid(UUID.randomUUID());
         orderEvent.setPayload(createRandomLetterString(MAX_LENGTH_512));
