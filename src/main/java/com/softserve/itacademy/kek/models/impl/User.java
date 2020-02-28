d3e0d7aae0ac26f5f73ce52eadaf21e90ad3a8cd
Olexiy Lyaschenko <lyaschenko2021@gmail.com>
2020-02-28 11:21:00 +0200

    Issue: https://gitlab.com/kv.061.java/project_kv_061/issues/38 Changed TokenAuthentication to UsernameAndPasswordAuthentication Changes: Changed TokenAuthentication to UsernameAndPasswordAuthentication, user now is mocked and we can get him from Principal/Authentication

--- src/main/java/com/softserve/itacademy/kek/models/impl/User.java
+++ src/main/java/com/softserve/itacademy/kek/models/impl/User.java
@@ -15,21 +15,16 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.softserve.itacademy.kek.models.IUser;
 import com.softserve.itacademy.kek.models.IUserDetails;
 
 @Entity
 @Table(name = "obj_user")
-public class User implements IUser, Serializable, org.springframework.security.core.userdetails.UserDetails {
+public class User extends AbstractEntity implements IUser, Serializable {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     List<Identity> identityList = new ArrayList<>();
@@ -42,25 +37,31 @@ public class User implements IUser, Serializable, org.springframework.security.c
     @NotNull
     @Column(name = "guid", nullable = false, unique = true)
     private UUID guid;
+
     @Email
     @NotNull
     @Size(min = 1, max = 256)
     @Column(name = "email", nullable = false, unique = true, length = 256)
     private String email;
+
     @NotNull
     @Size(min = 1, max = 256)
     @Column(name = "phone_number", nullable = false, unique = true, length = 256)
     private String phoneNumber;
+
     @NotNull
     @Size(min = 1, max = 256)
     @Column(name = "name", nullable = false, length = 256)
     private String name;
+
     @NotNull
     @Size(min = 1, max = 256)
     @Column(name = "nickname", nullable = false, unique = true, length = 256)
     private String nickname;
+
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER, optional = false)
     private UserDetails userDetails;
+
     @OneToOne(mappedBy = "tenantOwner", cascade = {CascadeType.REMOVE}, fetch = FetchType.LAZY, orphanRemoval = true)
     private Tenant tenant;
 
@@ -155,41 +156,6 @@ public class User implements IUser, Serializable, org.springframework.security.c
     public void removeIdentity(Identity identity) {
         identityList.remove(identity);
         identity.setUser(null);
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
     }
 
     @Override
