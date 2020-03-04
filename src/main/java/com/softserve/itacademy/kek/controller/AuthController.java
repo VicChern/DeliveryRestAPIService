5f505e5f7fba5521c5d4599d64fb5826710c3d6e
Olexiy Lyaschenko <lyaschenko2021@gmail.com>
2020-03-04 15:47:52 +0200

    Issue: https://gitlab.com/kv.061.java/project_kv_061/issues/38 Refactored Auth0Controller Created new AuthenticationServiceImpl for Auth0Controller, created AuthenticatedUserDto for UserDetails

--- src/main/java/com/softserve/itacademy/kek/controller/AuthController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/AuthController.java
@@ -4,117 +4,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import com.auth0.AuthenticationController;
-import com.auth0.Tokens;
-import com.auth0.jwt.JWT;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.softserve.itacademy.kek.security.TokenAuthentication;
-import com.softserve.itacademy.kek.security.TokenUtils;
-import com.softserve.itacademy.kek.services.impl.UserDetailsServiceImpl;
+import com.softserve.itacademy.kek.dto.AuthenticatedUserDto;
+import com.softserve.itacademy.kek.services.IAuthenticationService;
 
 @RestController
-@PropertySource("classpath:server.properties")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthController extends DefaultController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    private AuthenticationController controller;
+    private final IAuthenticationService authenticationService;
 
-    @Value(value = "${redirect.from.auth0}")
-    private String redirectAuth0URL;
-
-    @Value(value = "${redirect.on.fail}")
-    private String redirectOnFail;
-
-    @Value(value = "${redirect.on.success}")
-    private String redirectOnSuccess;
-
-    @Value(value = "${redirect.after.success.logout}")
-    private String redirectAfterSuccessLogout;
+    public AuthController(IAuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
     @GetMapping(path = "/login")
     protected void login(HttpServletRequest request, HttpServletResponse response) {
         logger.info("Performing login, request = {}", request);
 
-        final String returnTo = createRedirectUrl(request.getScheme(), request.getServerName(),
-                request.getServerPort(), redirectAfterSuccessLogout);
-
-        final String authorizeUrl = controller.buildAuthorizeUrl(request, response, returnTo)
-                .withScope("openid profile email")
-                .build();
-
-        try {
-            logger.debug("trying to redirect to authorizeUrl = {}", authorizeUrl);
-            response.sendRedirect(authorizeUrl);
-
-        } catch (IOException e) {
-            logger.error("Failed to redirect to authorizeUrl {}, {}", authorizeUrl, e);
-        }
+        authenticationService.redirectToAuth0Login(request, response);
     }
 
     @RequestMapping(path = "/callback")
     protected void getCallback(HttpServletRequest request, HttpServletResponse response) throws IOException {
         logger.info("Entered to getCallBack method req = {}", request);
 
-        try {
-            final Tokens tokens = controller.handle(request, response);
-            final TokenAuthentication tokenAuth = new TokenAuthentication(JWT.decode(tokens.getIdToken()));
-
-            final UserDetailsService userDetailsService = new UserDetailsServiceImpl();
-            final UserDetails userDetails = userDetailsService.loadUserByUsername(tokenAuth.getClaims().get("email").asString());
-
-            final UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                    userDetails, null, userDetails.getAuthorities());
-
-            usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-
-            logger.info("User was authenticated successfully");
-
-            response.sendRedirect(redirectOnSuccess);
-        } catch (Exception e) {
-            logger.error("Error while authentication", e);
-
-            SecurityContextHolder.clearContext();
-            response.sendRedirect(redirectOnFail);
-        }
+        authenticationService.authenticateUser(request, response);
     }
 
     @GetMapping(path = "/profile")
-    protected ResponseEntity<String> profile(Authentication authentication, Principal principal) {
+    @PreAuthorize("hasRole('TENANT') or hasRole('USER') or hasRole('ACTOR')")
+    protected ResponseEntity<String> profile(Authentication authentication) {
 
-        JSONObject json = new JSONObject();
-        return ResponseEntity.ok(json.toString());
-    }
+        final AuthenticatedUserDto authenticatedUserDto = (AuthenticatedUserDto) authentication.getPrincipal();
 
-
-    private String createRedirectUrl(String scheme, String serverName, int serverPort, String afterSuccessfulRedirect) {
-        String returnTo = scheme + "://" + serverName;
-
-        if ((scheme.equals("http") && serverPort != 80) ||
-                (scheme.equals("https") && serverPort != 443)) {
-            returnTo += ":" + serverPort;
-        }
-
-        returnTo += afterSuccessfulRedirect;
-
-        return returnTo;
+        return ResponseEntity.ok(authenticatedUserDto.toString());
     }
 
 
--- src/main/java/com/softserve/itacademy/kek/controller/OrderController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,6 +37,7 @@ import com.softserve.itacademy.kek.services.IOrderService;
 
 @RestController
 @RequestMapping(path = "/orders")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class OrderController extends DefaultController {
     private final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
