cdfc594f7f4e904465d9afadcf93ea9dcd3f57fd
Galyna Klymenko <gklymenko@gmail.com>
2020-03-04 14:36:30 +0200

    Deleted empty unit test for sseController that was changed for IT test

--- src/main/java/com/softserve/itacademy/kek/controller/DefaultController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/DefaultController.java
@@ -6,13 +6,16 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.softserve.itacademy.kek.dto.ErrorListDto;
 import com.softserve.itacademy.kek.exception.ServiceException;
+import com.softserve.itacademy.kek.exception.TrackingException;
 
 @RestController
 public class DefaultController {
