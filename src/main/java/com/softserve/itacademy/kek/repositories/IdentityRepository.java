c8cafbb3fe63630a016e1aa0f5b4a2b3ba22790b
IShostak <ihor.shostak1@gmail.com>
2020-03-19 17:38:14 +0200

    Added methods to identity service
    Added identity type to savePassword method
    Added delete, get, update methods(in progress)

--- src/main/java/com/softserve/itacademy/kek/repositories/IdentityRepository.java
+++ src/main/java/com/softserve/itacademy/kek/repositories/IdentityRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.softserve.itacademy.kek.models.impl.Identity;
 
 public interface IdentityRepository extends JpaRepository<Identity, Long> {
+
+        Identity findKeyIdentity(String email);
 }
