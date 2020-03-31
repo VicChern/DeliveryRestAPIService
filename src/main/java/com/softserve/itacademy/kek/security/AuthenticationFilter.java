28a02a923f4e664220e29c6cfead48411eaa36fe
IShostak <ihor.shostak1@gmail.com>
2020-03-31 21:13:30 +0300

    Authentication controller changes
    Changed token (removed user details)
    Changed authcontroller (changed userdetails to email) cause exception in profile
    Signin controller now returns token in body(not in cookies)
    
    TODO:
    Change data parsing in sign in controller
    Solve bug with token(do we need user details or not)
    Change web security configuration(now signin and login need token)

--- src/main/java/com/softserve/itacademy/kek/security/AuthenticationFilter.java
+++ src/main/java/com/softserve/itacademy/kek/security/AuthenticationFilter.java
@@ -4,15 +4,22 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
-import java.util.Optional;
+import java.util.List;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import com.softserve.itacademy.kek.models.impl.UserDetails;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
@@ -25,11 +32,24 @@ public class AuthenticationFilter extends AbstractAuthenticationProcessingFilter
     @Override
     public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException {
 
-        Optional tokenParam = Optional.ofNullable(httpServletRequest.getHeader(AUTHORIZATION));
         String token = httpServletRequest.getHeader(AUTHORIZATION);
-        Authentication requestAuthentication = new UsernamePasswordAuthenticationToken(token, token);
-        return getAuthenticationManager().authenticate(requestAuthentication);
 
+        Claims claims = Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary("abc123"))
+                .parseClaimsJws(token).getBody();
+
+        String roles = claims.get("authorities").toString()
+                .replaceAll("[\\[\\]]", "")
+                .replaceAll("\\{authority=", "")
+                .replaceAll("\\}", "");
+
+        List<GrantedAuthority> list = AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
+
+        //TODO: investigate userdetails in requestAuthentication? and data parsing from token
+
+        Authentication requestAuthentication = new UsernamePasswordAuthenticationToken(claims.get("email"), null ,  list);
+
+        return getAuthenticationManager().authenticate(requestAuthentication);
     }
 
     @Override
