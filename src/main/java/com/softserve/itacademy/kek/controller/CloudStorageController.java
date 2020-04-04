bb9015b004b92bd2630e2c588aeeb979b06ac9a9
Valentyna Pivnytska <pivnytska@gmail.com>
2020-04-04 18:33:50 +0300

    [79] - Changed CloudStorageService methods

--- src/main/java/com/softserve/itacademy/kek/controller/CloudStorageController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/CloudStorageController.java
@@ -1,10 +1,14 @@
 package com.softserve.itacademy.kek.controller;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,36 +20,91 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.softserve.itacademy.kek.controller.utils.KekMappingValues;
 import com.softserve.itacademy.kek.controller.utils.KekMediaType;
+import com.softserve.itacademy.kek.dto.DetailsDto;
+import com.softserve.itacademy.kek.dto.ListWrapperDto;
+import com.softserve.itacademy.kek.dto.UserDetailsWithImageDto;
 import com.softserve.itacademy.kek.dto.UserDto;
+import com.softserve.itacademy.kek.dto.UserWithImageDto;
+import com.softserve.itacademy.kek.models.IUser;
+import com.softserve.itacademy.kek.models.services.ICloudStorageObject;
 import com.softserve.itacademy.kek.services.ICloudStorageService;
+import com.softserve.itacademy.kek.services.IUserService;
 
 @RestController
 @RequestMapping(path = "/users/cloud")
 public class CloudStorageController {
-    private final Logger logger = LoggerFactory.getLogger(CloudStorageController.class);
+    private final static Logger logger = LoggerFactory.getLogger(CloudStorageController.class);
 
     private final ICloudStorageService cloudStorageService;
+    private final IUserService userService;
 
     @Autowired
-    public CloudStorageController(ICloudStorageService cloudStorageService) {
+    public CloudStorageController(ICloudStorageService cloudStorageService, IUserService userService) {
         this.cloudStorageService = cloudStorageService;
+        this.userService = userService;
     }
 
-    @GetMapping(value = KekMappingValues.GUID, produces = KekMediaType.USER)
+    @GetMapping(value = KekMappingValues.GUID, produces = KekMediaType.USER_WITH_IMAGE)
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity getData(@PathVariable String guid) {
-        return null;
+    public ResponseEntity<UserWithImageDto> getData(@PathVariable String guid) {
+        ICloudStorageObject cloudStorageObject = cloudStorageService.getCloudStorageObject(guid);
+
+        IUser user = userService.getByGuid(UUID.fromString(guid));
+
+        UserWithImageDto userWithImageDto = transformUser(user, cloudStorageObject);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userWithImageDto);
     }
 
-    @GetMapping(produces = KekMediaType.USER_LIST)
+    @GetMapping(value = "/bucket", produces = KekMediaType.USER_WITH_IMAGE)
     @PreAuthorize("hasRole('TENANT')")
-    public ResponseEntity getDataList() {
-        return null;
+    public ResponseEntity<ListWrapperDto<UserWithImageDto>> getDataList(@PathVariable String bucket) {
+        List<ICloudStorageObject> dataList = cloudStorageService.getCloudStorageObjects(bucket);
+
+        ListWrapperDto<UserWithImageDto> users = new ListWrapperDto<>();
+
+        for (ICloudStorageObject cloudStorageObject : dataList) {
+            IUser user = userService.getByGuid(UUID.fromString(cloudStorageObject.getGuid()));
+
+            users.addKekItem(transformUser(user, cloudStorageObject));
+        }
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(users);
     }
 
-    @PostMapping(consumes = KekMediaType.USER, produces = KekMediaType.USER)
+    @PostMapping(consumes = KekMediaType.USER_WITH_IMAGE, produces = KekMediaType.USER)
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity uploadData(@RequestBody @Valid UserDto newUserDto) {
-        return null;
+    public ResponseEntity<UserDto> uploadData(@RequestBody @Valid UserWithImageDto userWithImage) {
+        UserDetailsWithImageDto userDetails = userWithImage.getUserDetailsWithImage();
+        String data = userDetails.getImage();
+
+        byte[] encodedByte = Base64.decodeBase64(data.getBytes());
+
+        ICloudStorageObject cloudStorageObject = cloudStorageService.uploadBinaryData(encodedByte, userWithImage.getGuid());
+        String url = cloudStorageObject.getUrlString();
+
+        UserDto userDto = transformUser(userWithImage, url);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userDto);
+    }
+
+    private UserDto transformUser(UserWithImageDto userWithImage, String url) {
+        DetailsDto userDetailsDto = new DetailsDto(url, userWithImage.getUserDetailsWithImage().getPayload());
+
+        return new UserDto(userWithImage.getGuid(), userWithImage.getName(), userWithImage.getNickname(),
+                userWithImage.getEmail(), userWithImage.getPhoneNumber(), userDetailsDto);
+    }
+
+    private UserWithImageDto transformUser(IUser user, ICloudStorageObject cloudStorageObject) {
+        UserDetailsWithImageDto userDetails = new UserDetailsWithImageDto(user.getUserDetails().getPayload(),
+                Base64.encodeBase64String(cloudStorageObject.getDataBytes()));
+
+        return new UserWithImageDto(user.getGuid(), user.getName(), user.getNickname(),
+                user.getEmail(), user.getPhoneNumber(), userDetails);
     }
 }
