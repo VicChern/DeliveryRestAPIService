fd2e2a9ff8dd16865adb4a27fa91b195a273fec4
Valentyna Pivnytska <pivnytska@gmail.com>
2020-04-07 21:39:24 +0300

    [81] - Changed mapping in AuthController class

--- src/main/java/com/softserve/itacademy/kek/controller/AuthController.java
+++ src/main/java/com/softserve/itacademy/kek/controller/AuthController.java
@@ -18,11 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.softserve.itacademy.kek.controller.utils.KekMappingValues;
 import com.softserve.itacademy.kek.controller.utils.KekMediaType;
 import com.softserve.itacademy.kek.dto.UserDto;
+import com.softserve.itacademy.kek.mappers.IUserMapper;
 import com.softserve.itacademy.kek.models.IUser;
 import com.softserve.itacademy.kek.services.IAuthenticationService;
 import com.softserve.itacademy.kek.services.IUserService;
-
-import static com.softserve.itacademy.kek.mapper.UserMapper.toUserDto;
 
 @RestController
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -63,7 +62,7 @@ public class AuthController extends DefaultController {
 
         final IUser user = userService.getByEmail(email);
 
-        UserDto userDto = toUserDto(user);
+        UserDto userDto = IUserMapper.INSTANCE.toUserDto(user);
 
         logger.debug("Performed profile request : {}", userDto);
         return ResponseEntity.ok(userDto);
